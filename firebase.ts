c4b46be302b83e7dc8ae306436806b772b9aa217
General Dee <oshinowodare@gmail.com>
2025-11-22 15:52:25 +0100

    firebase storage/ Whatsapp redirect

--- firebase.ts
+++ firebase.ts
@@ -1,24 +1,21 @@
+"use client";
+
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from 'firebase/storage'
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDukoonQ4z-xd5jakKfjSRLIZn9KUheLww",
   authDomain: "originalmalyam.firebaseapp.com",
   projectId: "originalmalyam",
   storageBucket: "originalmalyam.firebasestorage.app",
   messagingSenderId: "65847637845",
-  appId: "1:65847637845:web:35dfcb9627d98ec50ea1dc"
+  appId: "1:65847637845:web:35dfcb9627d98ec50ea1dc",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore()
-
-export const storage = getStorage(app)
-
-export const auth = getAuth();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
