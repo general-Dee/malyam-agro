02e29f7912718eaffe4390c5f12d6c21a05ed1b7
General Dee <oshinowodare@gmail.com>
2025-11-23 13:49:52 +0100

    Vercel environment variables update

--- firebase.ts
+++ firebase.ts
@@ -5,14 +5,25 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDukoonQ4z-xd5jakKfjSRLIZn9KUheLww",
+//   authDomain: "originalmalyam.firebaseapp.com",
+//   projectId: "originalmalyam",
+//   storageBucket: "originalmalyam.firebasestorage.app",
+//   messagingSenderId: "65847637845",
+//   appId: "1:65847637845:web:35dfcb9627d98ec50ea1dc",
+// };
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDukoonQ4z-xd5jakKfjSRLIZn9KUheLww",
-  authDomain: "originalmalyam.firebaseapp.com",
-  projectId: "originalmalyam",
-  storageBucket: "originalmalyam.firebasestorage.app",
-  messagingSenderId: "65847637845",
-  appId: "1:65847637845:web:35dfcb9627d98ec50ea1dc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 
